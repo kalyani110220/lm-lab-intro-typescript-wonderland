45d3a3dd85a10cf9f40aa55be17b50fbae5263fb
kalyani <kalyani.nelluri54@gmail.com>
2023-06-03 01:04:12 +0100

    Implementation Activity 6

--- src/chapter_6/chapter_6_wake_up.ts
+++ src/chapter_6/chapter_6_wake_up.ts
@@ -47,6 +47,14 @@ export function wakeUp(): void {
 }
 
 function tryToWakeUp(): WakeUp {
-	// 👉 FIXME ❌
-	return {};
+	
+	return {
+		wake: {
+			wake: {
+				wake: {
+					canWake: 'Yes',
+					},
+				},
+			},
+		};
 }
