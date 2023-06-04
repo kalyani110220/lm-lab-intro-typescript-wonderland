9710ca148ddf784431068337ace21e999f763643
kalyani <kalyani.nelluri54@gmail.com>
2023-06-04 08:25:01 +0100

    try toimplementedchapter7Alice went back to home to meet mom and buy some groceries

--- src/chapter_7/chapter_7_happliy_went home.ts
+++ src/chapter_7/chapter_7_happliy_went home.ts
@@ -1,41 +1,85 @@
 import { endAdventure, haveAdventures } from '../..';
 import { askQuestion, clear, print } from '../ui/console';
 
-function alicesAdventure(): void {
-    console.log("Alice starts her adventure.");
-    console.log("Alice and her mom walk to the grocery store.");
-  
-    // Implement your adventure logic here
-    const groceryList: string[] = ["fruits", "vegetables"];
-  
-    console.log("Alice and her mom arrive at the grocery store.");
-    console.log("Alice and her mom enter the store.");
-  
-    for (const item of groceryList) {
-      console.log(`Alice and her mom select ${item}.`);
-    }
-  
-    console.log("Alice and her mom proceed to the checkout.");
-  
-    // Implement card payment logic
-    const cardNumber: string = "1234 5678 9012 3456";
-    const cardHolderName: string = "Alice's Mom";
-    const expirationDate: string = "12/25";
-    const cvv: string = "123";
-  
-    function makePayment(amount: number): void {
-      console.log(`Card payment of $${amount} initiated.`);
-      // Implement the logic to process the payment with the given card details
-      console.log(`Card payment of $${amount} successful.`);
-    }
-  
-    const totalAmount: number = 50; // Total amount to be paid
-  
-    makePayment(totalAmount);
-  
-    console.log("Alice and her mom complete their grocery shopping.");
-    console.log("Alice and her mom head back home with their groceries.");
-    console.log("They are happy and excited about the delicious fruits and vegetables they bought!");
-  }
-  
-  alicesAdventure();
+interface WakeUp {
+  wakeUp: () => void;
+}
+
+interface GoHome {
+  goHome: (callback: () => void) => void;
+}
+
+interface BuyGroceries {
+  buyGroceries: () => void;
+}
+
+// ...
+
+export function wakeUp(): void {
+  clear(true);
+  print('Wait... was this all a dream?');
+
+  const awoken: WakeUp = {
+    wakeUp: () => {
+      print('You have awoken in your garden 🛏 What a lovely dream.');
+      print('Although...❓❓❓');
+      print('What are these tarts doing here?! 🥧🥧🥧🥧🥧🥧 🤔');
+
+      print('✅ CONGRATULATIONS! You successfully made it through Wonderland! 🥳');
+
+      goHome();
+    },
+  };
+
+  awoken.wakeUp();
+}
+
+function goHome(): void {
+  const goHome: GoHome = {
+    goHome: (callback: () => void) => {
+      print('Alice goes back home to meet her mom.');
+      print("Alice: Hi, Mom! I'm back from my adventure.");
+
+      callback();
+    },
+  };
+
+  goHome.goHome(buyGroceries);
+}
+
+function buyGroceries(): void {
+  const buyGroceries: BuyGroceries = {
+    buyGroceries: () => {
+      clear(true);
+      print("Alice and her mom went to the grocery store.");
+
+      // Logic for buying fruits and vegetables
+      const fruits: string[] = ['apple', 'banana', 'orange'];
+      const vegetables: string[] = ['carrot', 'tomato', 'broccoli'];
+
+      print("Alice and her mom are buying groceries...");
+      print(`Fruits: ${fruits.join(', ')}`);
+      print(`Vegetables: ${vegetables.join(', ')}`);
+
+      print("Alice and her mom say hello to the shopkeeper.");
+
+      // Make a card payment
+      const totalAmount: number = 50; // Total amount in pounds
+      const cardNumber: string = "1234567890"; // Card number
+
+      print(`Alice and her mom make a card payment of ${totalAmount} pounds.`);
+      print(`Card Number: ${cardNumber}`);
+
+      print("Payment successful! Groceries are bought.");
+
+      // ...
+
+      print("Alice and her mom head back home with their groceries.");
+      print("They are happy and excited about the delicious food they bought!");
+
+      return askQuestion("Press ENTER to start your next adventure! ", haveAdventures);
+    },
+  };
+
+  buyGroceries.buyGroceries();
+}
